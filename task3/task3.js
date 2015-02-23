97be7ce0f024a9dc0d0661a7a32c0777dd67640f
Lindani <lindani@projectcodex.co>
2015-02-23 12:32:35 +0200

    task3 is done(update)

---
+++ task3/task3.js
@@ -0,0 +1,8 @@
+function green()
+    	{
+    		document.getElementById("one").style.backgroundColor = "green";
+    	}
+function red()
+    	{
+    		document.getElementById("one").style.backgroundColor = "red";
+    	}
